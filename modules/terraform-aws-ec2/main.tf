8966d3cb2e20fe2b61919cdf0247db161b1a200f
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-04 09:42:54 +0530

    Update main.tf

--- modules/terraform-aws-ec2/main.tf
+++ modules/terraform-aws-ec2/main.tf
@@ -58,39 +58,15 @@ resource "aws_instance" "public_instances" {
   # user_data = local.user_data_files[0]
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update -y
-              apt-get install -y \
-                apt-transport-https \
-                ca-certificates \
-                curl \
-                software-properties-common \
-                gnupg \
-                lsb-release
-
-              # Install Docker
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-              echo \
-                "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-                $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-              apt-get update -y
-              apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+              sudo apt-get update -y
+              sudo apt-get install docker.io -y
 
               # Enable Docker
               systemctl start docker
               systemctl enable docker
 
-              # Pull specific OpenProject Docker image
-              docker pull openproject/openproject:15
-
               # Run OpenProject container with specified environment variables
-              docker run -d --name openproject \
-                -e OPENPROJECT_SECRET_KEY_BASE=secret \
-                -e OPENPROJECT_HOST__NAME=localhost:8080 \
-                -e OPENPROJECT_HTTPS=false \
-                -e OPENPROJECT_DEFAULT__LANGUAGE=en \
-                -p 8080:80 \
-                openproject/openproject:15
+              docker run -d -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=0.0.0.0:80 -e OPENPROJECT_HTTPS=false openproject/community:12
               EOF
 
   tags = {
