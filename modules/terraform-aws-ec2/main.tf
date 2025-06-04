6fbb1d8e5cfaf356ab935f9f038022fa81109bef
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-04 09:47:45 +0530

    Update main.tf

--- modules/terraform-aws-ec2/main.tf
+++ modules/terraform-aws-ec2/main.tf
@@ -60,12 +60,9 @@ resource "aws_instance" "public_instances" {
               #!/bin/bash
               sudo apt-get update -y
               sudo apt-get install docker.io -y
-
-              # Enable Docker
               systemctl start docker
               systemctl enable docker
-
-              # Run OpenProject container with specified environment variables
+              sleep 10
               docker run -d -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=0.0.0.0:80 -e OPENPROJECT_HTTPS=false openproject/community:12
               EOF
 
