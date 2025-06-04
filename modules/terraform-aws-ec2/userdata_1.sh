d998b965235467e9b7ccd6f2bdc9f7b3922d7eab
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-04 12:15:17 +0530

    Update userdata_1.sh

--- modules/terraform-aws-ec2/userdata_1.sh
+++ modules/terraform-aws-ec2/userdata_1.sh
@@ -4,4 +4,4 @@
               systemctl start docker
               systemctl enable docker
               sleep 10
-              docker run -d -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=0.0.0.0:80 -e OPENPROJECT_HTTPS=false openproject/community:1
+              docker run -d -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=0.0.0.0:80 -e OPENPROJECT_HTTPS=false openproject/community:12
